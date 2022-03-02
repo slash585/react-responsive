3d91a8918f81e0a54439b2cd6e2cab3582f7a9b0
slash585 <slsh585@gmail.com>
2022-03-03 02:35:43 +0300

    Add home view and hero component

---
+++ src/views/Home/home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import HeroSection from "../../components/Hero/heroSection"
+import '../../App.css'
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
+
+export default Home
